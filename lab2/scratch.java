caa58cf9ea21f6490618ef4bf24021ce17dcefa9
Student <abdulhalilmyhammad@gmail.com>
2026-01-29 13:58:25 +0500

    asd

---
+++ lab2/scratch.java
@@ -0,0 +1,23 @@
+class Scratch {
+    public static void main(String[] args) {
+        
+    }
+}
+public class PrimitiveExample {
+
+    public static void main(String[] args) {
+        // Переменные примитивных типов
+        int age = 20;
+        double height = 1.75;
+        boolean isStudent = true;
+
+        // Константа
+        final String UNIVERSITY = "МГУ";
+
+        // Вывод значений в консоль с пояснениями
+        System.out.println("Возраст: " + age + " лет");
+        System.out.println("Рост: " + height + " метра");
+        System.out.println("Является студентом: " + isStudent);
+        System.out.println("Университет (константа): " + UNIVERSITY);
+    }
+}
